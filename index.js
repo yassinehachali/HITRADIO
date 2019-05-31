faa925a6b79f7b4d9fa1e9fb509fc64fd1b27df9
yassinehachali <33522063+yassinehachali@users.noreply.github.com>
2019-05-31 03:41:19 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const Discord=require('discord.js')
+const bot=new Discord.Client()
+
+var dispatcher;
+
+
+
+bot.on('message',function(message){
+if(message.content==='!Tasir'){
+    if(message.member.voiceChannel){
+    message.member.voiceChannel.join().then(connection =>{
+        dispatcher=connection.playArbitraryInput("http://hitradio-maroc.ice.infomaniak.ch/hitradio-maroc-128.mp3");
+    });
+   }else{
+    message.reply("Wa Dkhul lchi channel a Hamadi");
+   }
+}else if(message.content==='!sirt7wa'){
+    message.member.voiceChannel.leave();
+    message.reply("Hana Ghadi a weld l9a7ba a berred");
+}
+})
+ 
+
+
+ bot.login('NTgzNzk3MTk0MjY4ODY4NjE4.XPBm6Q.hyKjgn7Dr3d5hRubqopgYqCWRqs')
